78c50a7ce6065f1162cff86a309cbc5391a298a2
jiftechnify <jiftech.stlfy@gmail.com>
2024-10-11 13:03:17 +0900

    update logic to use api.song.link

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,47 @@ import { cache } from "hono/cache";
 import { cors } from "hono/cors";
 import { HTTPException } from "hono/http-exception";
 
-type SongWhipResp = {
-  name?: string;
-  artists?: {
-    name?: string;
-  }[];
-  url?: string;
-};
-
-type MusicData = {
+type SongData = {
   url: string;
   title?: string;
-  artists?: string[];
+  artist?: string;
+};
+
+type SongLinkEntity = {
+  title?: string;
+  artistName?: string;
+  apiProvider: string;
+};
+
+type SongLinkResp = {
+  entityUniqueId: string;
+  pageUrl: string;
+  entitiesByUniqueId: Record<string, SongLinkEntity>;
+};
+
+const extractSongData = (slResp: SongLinkResp): SongData => {
+  const url = slResp.pageUrl;
+  const spotifyEntity = Object.values(slResp.entitiesByUniqueId).find((e) =>
+    e.apiProvider === "spotify"
+  );
+  if (spotifyEntity !== undefined) {
+    return {
+      url,
+      title: spotifyEntity.title,
+      artist: spotifyEntity.artistName,
+    };
+  }
+
+  const entity = slResp.entitiesByUniqueId[slResp.entityUniqueId];
+  if (entity !== undefined) {
+    return {
+      url,
+      title: entity.title,
+      artist: entity.artistName,
+    };
+  }
+
+  return { url };
 };
 
 const app = new Hono();
@@ -23,49 +52,48 @@ app.get(
   "*",
   cors({
     origin: (origin) =>
-      origin.startsWith("https://nostatus") && origin.endsWith(".vercel.app") || origin.includes("localhost:")
+      origin.startsWith("https://nostatus") && origin.endsWith(".vercel.app") ||
+        origin.includes("localhost:")
         ? origin
         : undefined,
     allowMethods: ["GET"],
-  })
+  }),
 );
 
 app.get(
   "*",
   cache({
     cacheName: "cache",
-  })
+  }),
 );
 
 app.get("/", async (c) => {
-  const musicLinkUrl = c.req.query("url");
+  const shareUrl = c.req.query("url");
+  if (shareUrl === undefined) {
+    throw new HTTPException(400, { message: "Bad Request" });
+  }
+  const userCountry = c.req.query("country") ?? "US";
 
-  console.log(
-    "forwarding request to SongWhip API... (music URL: %s)",
-    musicLinkUrl
-  );
-  const resp = await fetch("https://songwhip.com/", {
-    method: "POST",
-    body: JSON.stringify({ url: musicLinkUrl }),
-  });
+  const url = new URL("https://api.song.link/v1-alpha.1/links");
+  const params = new URLSearchParams();
+  params.set("url", shareUrl);
+  params.set("userCountry", userCountry);
+  params.set("songIfSingle", "true");
+  url.search = params.toString();
+
+  console.log("forwarding request to:", url.toString());
+
+  const resp = await fetch(url);
   if (!resp.ok) {
     throw new HTTPException(resp.status, { message: resp.statusText });
   }
 
-  const swResp = (await resp.json()) as SongWhipResp;
-  if (swResp.url === undefined) {
-    throw new HTTPException(400, { message: "Bad Request" });
-  }
+  const slResp = (await resp.json()) as SongLinkResp;
 
-  const musicData: MusicData = {
-    url: swResp.url,
-    title: swResp.name,
-    artists:
-      swResp.artists?.flatMap((a) => (a.name ? [a.name] : [])) ?? undefined,
-  };
-  console.log("music data for %s: %O", musicLinkUrl, musicData);
+  const songData = extractSongData(slResp);
+  console.log("song data for %s: %O", shareUrl, songData);
 
-  return c.json(musicData);
+  return c.json(songData);
 });
 
 export default app;
